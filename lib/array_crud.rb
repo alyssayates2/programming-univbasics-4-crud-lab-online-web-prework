e2c575337cdbdd150a361d4c7af616fc369412a5
Alyssa Yates <bosslys@icloud.com>
2019-08-21 21:26:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,8 +6,8 @@ def create_an_array
   [0 , 1 , 2 , 3 ]
 end
 
-def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array(array, element).push(4)
+def add_element_to_end_of_array(array, element) << 4
+  
 end
 
 def add_element_to_start_of_array(array, element)
